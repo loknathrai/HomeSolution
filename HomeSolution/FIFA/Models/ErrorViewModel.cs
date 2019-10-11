381c21107623a24b51edd9cfc764ac45f3ce4d64
loknathrai <56198449+loknathrai@users.noreply.github.com>
2019-10-11 11:14:20 +0100

    jj

---
+++ HomeSolution/FIFA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FIFA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
